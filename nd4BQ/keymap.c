7037e3acfa2cde9dc63437383b851241b9c740b6
sven-dibowski <30394136+sven-dibowski@users.noreply.github.com>
2024-12-05 18:44:23 +0100

    Update keymap.c

--- nd4BQ/keymap.c
+++ nd4BQ/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "i18n.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
@@ -8,7 +9,9 @@ enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
 
-
+void matrix_scan_user(void) {
+  achordion_task();
+}
 
 enum tap_dance_codes {
   DANCE_0,
@@ -135,6 +138,7 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
