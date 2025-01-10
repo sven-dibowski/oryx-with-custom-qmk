47175e419d43689d4a5c579b9b24bf58c3f4e430
sven-dibowski <30394136+sven-dibowski@users.noreply.github.com>
2025-01-10 22:23:03 +0100

    Update achordion.c

--- nd4BQ/features/achordion.c
+++ nd4BQ/features/achordion.c
@@ -342,7 +342,8 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
 // By default, the timeout is 1000 ms for all keys.
 __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
 switch (tap_hold_keycode) {
-    case LT(2,KC_SPACE):
+  case LT(2,KC_SPACE):
+  case LT(1,KC_ENTER):  
       return 0;  // Bypass Achordion for these keys.
   }
    
